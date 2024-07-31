c6f4891c048cd7b3008c5dfee7c6bfdded6c1474
Amila98 <amilamadhujith@gmail.com>
2024-07-31 14:42:24 +0530

    added new routes

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { verifyStaff, loginStaff, changePassword } = require('../controllers/staffController');
+const { verifyStaff, loginStaff, changePassword, viewStaffDetails, updateStaffDetails } = require('../controllers/staffController');
 const auth = require('../middleware/authMiddleware');
 
 // Route to verify staff
 router.get('/verify/:token', verifyStaff);
 router.post('/login', loginStaff);
 router.post('/change-password', changePassword);
-
+router.put('/edit-details', auth.authMiddleware, updateStaffDetails);
+router.get('/details', auth.authMiddleware, viewStaffDetails);
 
 module.exports = router;
