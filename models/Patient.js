b4481c2cb57c37141066cfda61018fdadcfdb540
Amila98 <amilamadhujith@gmail.com>
2024-08-01 09:10:00 +0530

    updated

--- models/Patient.js
+++ models/Patient.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const PatientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
-  role: { type: String,enum: ['patient', 'doctor', 'admin', 'staff'], default: 'patient' },
+  role: { type: String, enum: ['patient', 'doctor', 'admin', 'staff'], default: 'patient' },
   verificationToken: { type: String },
-  mustChangePassword: { type: Boolean,default: false,}
+  mustChangePassword: { type: Boolean, default: false }
 });
 
-
 // Method to compare hashed passwords
-UserSchema.methods.comparePassword = async function (candidatePassword) {
+PatientSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Patient', PatientSchema);
