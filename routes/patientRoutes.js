7272a76864754a422392f1dc0cb96fbd64683097
Amila98 <amilamadhujith@gmail.com>
2024-08-01 09:10:18 +0530

    new routes

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,18 +1,27 @@
-// routes/patientRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerPatient } = require('../controllers/patientController');
-const { loginUser } = require('../controllers/authController');
-const { requestPasswordReset, resetPassword}  =  require('../controllers/patientController');
+const  {registerPatient, verify, loginUser, requestPasswordReset, resetPassword, viewPatientDetails, updatePatientDetails }= require('../controllers/patientController');
+const {authMiddleware} = require('../middleware/authMiddleware'); // Middleware to verify JWT token
 
-//
+// Patient registration route
 router.post('/register', registerPatient);
 
-// Patient login route (using generic login function)
+// Email verification route
+router.get('/verify/:token', verify);
+
+// Login route
 router.post('/login', loginUser);
 
+// Request password reset route
 router.post('/request-password-reset', requestPasswordReset);
+
+// Reset password route
 router.post('/reset-password/:token', resetPassword);
 
+// View patient personal information (protected route)
+router.get('/details', authMiddleware, viewPatientDetails);
+
+// Update patient personal information (protected route)
+router.put('/details', authMiddleware, updatePatientDetails);
 
 module.exports = router;
