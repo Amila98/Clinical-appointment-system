c323c4c5140a9772326cba54c562ef063b0e4755
Amila98 <amilamadhujith@gmail.com>
2024-08-02 11:11:57 +0530

    updated

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { registerDoctor,loginDoctor, changeDoctorPassword, viewDoctorDetails, updateDoctorDetails } = require('../controllers/doctorController');
 const auth = require('../middleware/authMiddleware'); // Middleware to authenticate the token
 
-router.post('/register', registerDoctor);
+router.post('/register/:token', registerDoctor);
 
 // Doctor login route
 router.post('/login', loginDoctor);
