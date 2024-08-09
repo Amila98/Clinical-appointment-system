d2e997c2e5d3bdac6c9c7cf43837b23395d8bdca
Amila98 <amilamadhujith@gmail.com>
2024-08-09 22:24:03 +0530

    updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 // middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
+// Middleware to authenticate the token
 const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
@@ -8,28 +9,34 @@ const authMiddleware = (req, res, next) => {
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Decoded JWT:", decoded); // Debugging log
         req.user = decoded;
         next();
     } catch (err) {
-        console.error(err.message);
+        console.error('Token verification failed:', err.message);
         res.status(401).json({ msg: 'Token is not valid', error: err.message });
     }
 };
-const roleCheck = (role) => {
+
+const roleCheck = (roles) => {
     return (req, res, next) => {
-        if (req.user.role !== role) {
+        console.log("User role:", req.user.role); // Debugging log
+        if (!roles.includes(req.user.role)) {
             return res.status(403).json({ msg: 'Access denied' });
         }
         next();
     };
 };
 
-// middleware/verifyMiddleware.js
+
 const verifyMiddleware = (req, res, next) => {
-    if (!req.user.isVerified) {
-      return res.status(403).json({ msg: 'Email not verified' });
-    }
-    next();
-  };
+  // Check if the user's email is verified.
+  if (!req.user.isVerified) {
+    // If the user's email is not verified, return a 403 status code and an error message.
+    return res.status(403).json({ msg: 'Email not verified' });
+  }
+  // If the user's email is verified, proceed to the next middleware function.
+  next();
+};
 
 module.exports = { authMiddleware, roleCheck, verifyMiddleware };
