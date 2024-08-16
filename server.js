e7349a1cf5678164cd89650469d8dce17df12507
Amila98 <amilamadhujith@gmail.com>
2024-08-16 09:30:04 +0530

    updated

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+const initPermissions = require('./utils/initPermissions');
+
 
 const patientRoutes = require('./routes/patientRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const doctorRoutes = require('./routes/doctorRoutes');
 const staffRoutes = require('./routes/staffRoutes');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 require('dotenv').config();
 
 
 const app = express();
+
+initPermissions();
 
 // Middleware
 app.use(express.json());
@@ -21,8 +27,7 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/doctor', doctorRoutes);
 app.use('/api/staff', staffRoutes);
 app.use('/api/auth', authRoutes);
-
-
+app.use('/api/user', userRoutes);
 
 
 // Connect to database and start server
