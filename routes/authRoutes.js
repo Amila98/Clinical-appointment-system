2160d2f93295d9177c5d1e1a7eb71c19eba72bdc
Amila98 <amilamadhujith@gmail.com>
2024-08-16 09:32:04 +0530

    new routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { forgotPassword, resetPassword } = require('../controllers/authController');
+const { forgotPassword, resetPassword, uploadProfilePicture } = require('../controllers/authController');
+const { authMiddleware, verifyMiddleware } = require('../middleware/authMiddleware');
+const { uploadMiddleware } = require('../middleware/uploadMiddleware');
+
 
 // Forgot password route
 router.post('/forgot-password', forgotPassword);
 
 // Reset password route
 router.post('/reset-password/:token', resetPassword);
+
+// Route to upload profile picture
+router.post('/upload-profile-picture', authMiddleware, verifyMiddleware,uploadMiddleware, uploadProfilePicture);
+
 
 module.exports = router;
