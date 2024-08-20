3bbf5fbee54008c361bc30a4e71fdd9c9d1c58aa
Amila98 <amilamadhujith@gmail.com>
2024-08-20 09:24:49 +0530

    added permissions

---
+++ utils/initPermissions.js
@@ -0,0 +1,73 @@
+// utils/initPermissions.js
+const Permission = require('../models/Permission');
+
+const initPermissions = async () => {
+    const permissions = [
+        {
+            role: 'Super Admin',
+            permissions: [
+                'create_user',
+                'update_user',
+                'delete_user',
+                'read_user',
+                'invite_doctor',
+                'verify_doctor',
+                'create_staff',
+                'change_user_email',
+                'change_admin_password',
+                'view_admin_details',
+                'update_admin_details',
+                'upload-profile-picture'
+            ]
+        },
+        {
+            role: 'Admin',
+            permissions: [
+                'create_user',
+                'update_user',
+                'delete_user',
+                'read_user',
+                'invite_doctor',
+                'verify_doctor',
+                'create_staff',
+                'change_user_email',
+                'change_admin_password',
+                'view_admin_details',
+                'update_admin_details',
+                'upload-profile-picture'
+            ]
+        },
+        {
+            role: 'Doctor',
+            permissions: [
+                'change_doctor_password',
+                'view_doctor_details',
+                'update_doctor_details',
+                'upload-profile-picture'
+            ]
+        },
+        {
+            role: 'Staff',
+            permissions: [
+                'view_staff_details',
+                'update_staff_details',
+                'upload-profile-picture',
+                'read_user'
+            ]
+        },
+        {
+            role: 'Patient',
+            permissions: [
+                'view_patient_details',
+                'update_patient_details',
+                'upload-profile-picture'
+            ]
+        }
+    ];
+
+    for (const perm of permissions) {
+        await Permission.findOneAndUpdate({ role: perm.role }, perm, { upsert: true });
+    }
+};
+
+module.exports = initPermissions;
