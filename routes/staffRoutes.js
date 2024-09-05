4ac158ba5d1bfd17f753f85b482de17828795df3
Amila98 <amilamadhujith@gmail.com>
2024-09-05 13:22:28 +0530

    initial commit

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { uploadMiddleware } = require('../middleware/uploadMiddleware');
-const { verifyStaff, loginStaff, changePassword, viewStaffDetails, updateStaffDetails } = require('../controllers/staffController');
-const auth = require('../middleware/authMiddleware');
+const jwt = require('jsonwebtoken');
+
+const { verifyStaff } = require('../controllers/staffController');
+const { authMiddleware,roleCheck } = require('../middleware/authMiddleware'); // Middleware to authenticate the token
+
+
+
+
+// Handle the verification and password setting (React will handle the UI)
+router.get('/verify/:token', (req, res) => {
+    const { token } = req.params;
+
+    try {
+        // Decode the token to ensure it's valid
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // If token is valid, respond with a success message or status code
+        res.status(200).json({ msg: 'Token is valid', token });
+    } catch (err) {
+        // If token is invalid or expired, respond with an error
+        res.status(400).json({ msg: 'Invalid or expired token' });
+    }
+});
 
 // Route to verify staff
 router.post('/verify/:token', verifyStaff);
-router.post('/login', loginStaff);
-router.post('/change-password', changePassword);
-router.get('/details', auth.authMiddleware, viewStaffDetails);
-router.put('/edit-details', auth.authMiddleware,uploadMiddleware, updateStaffDetails);
+
 
 
 module.exports = router;
