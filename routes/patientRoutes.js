f2bb570668bc9d030f8a9fb48ab8cb2e306f592c
Amila98 <amilamadhujith@gmail.com>
2024-09-08 19:34:47 +0530

    initial commit

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { uploadMiddleware } = require('../middleware/uploadMiddleware');
-const  {registerPatient, verify, loginUser, requestPasswordReset, resetPassword, viewPatientDetails, updatePatientDetails }= require('../controllers/patientController');
-const {authMiddleware} = require('../middleware/authMiddleware'); // Middleware to verify JWT token
+const { uploadProfilePicture } = require('../controllers/authController');
+const { uploadImageMiddleware,uploadMedicalFileMiddleware } = require('../middleware/uploadMiddleware');
+const  {registerPatient, verify, requestPasswordReset, resetPassword, updatePatientDetails }= require('../controllers/patientController');
+const {authMiddleware,roleCheck} = require('../middleware/authMiddleware'); // Middleware to verify JWT token
 
 // Patient registration route
-router.post('/register', registerPatient);
+router.post('/register',uploadMedicalFileMiddleware, registerPatient);
 
 // Email verification route
 router.get('/verify/:token', verify);
-
-// Login route
-router.post('/login', loginUser);
 
 // Request password reset route
 router.post('/request-password-reset', requestPasswordReset);
 
 // Reset password route
 router.post('/reset-password/:token', resetPassword);
-
-// View patient personal information (protected route)
-router.get('/details', authMiddleware, viewPatientDetails);
-
-// Update patient personal information (protected route)
-router.put('/details', authMiddleware,uploadMiddleware, updatePatientDetails);
 
 module.exports = router;
