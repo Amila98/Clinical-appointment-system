c4eb2ab6be0879866b35d9991bdc3b3398f03f1d
Amila98 <amilamadhujith@gmail.com>
2024-09-15 22:03:24 +0530

    updated

--- models/Invitation.js
+++ models/Invitation.js
@@ -14,6 +14,19 @@ const InvitationSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    expiresAt: {
+        type: Date,
+        required: false,
+    }
+});
+
+
+// Automatically set expiration date (24 hours from now)
+InvitationSchema.pre('save', function (next) {
+    if (!this.expiresAt) {
+        this.expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24 hours from now
+    }
+    next();
 });
 
 const Invitation = mongoose.model('Invitation', InvitationSchema);
