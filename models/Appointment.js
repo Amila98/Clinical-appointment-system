a0f576b9eef9f52815b99a2fc89335bf3823f739
Amila98 <amilamadhujith@gmail.com>
2024-09-21 21:05:54 +0530

    updated

--- models/Appointment.js
+++ models/Appointment.js
@@ -11,6 +11,11 @@ const AppointmentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Patient',  // Reference to the Patient model if you have one
     },
+
+    specialization: { type: mongoose.Schema.Types.ObjectId, ref: 'Specialization', required: true },
+
+    schedule: { type: mongoose.Schema.Types.ObjectId, ref: 'Schedule', required: true },
+
     day: {
         type: String,
         required: true,
@@ -19,10 +24,8 @@ const AppointmentSchema = new mongoose.Schema({
         start: { type: String, required: true },  // e.g., '09:00'
         end: { type: String, required: true }     // e.g., '09:30'
     },
-    slotIndex: {
-        type: Number,  // Index of the slot within the schedule
-        required: true
-    },
+
+    
     status: { 
         type: String, 
         enum: ['Scheduled', 'Completed', 'Cancelled'], 
