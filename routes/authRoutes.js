bafbc7617a523ab951fe7bbb78502ddee47a9903
Amila98 <amilamadhujith@gmail.com>
2024-09-24 09:46:19 +0530

    updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { uploadProfilePicture, changePassword } = require('../controllers/authController');
+const { uploadProfilePicture, changePassword, viewProfilePicture, deleteProfilePicture } = require('../controllers/authController');
 const { authMiddleware, roleCheck } = require('../middleware/authMiddleware');
-const { uploadImageMiddleware } = require('../middleware/uploadMiddleware');
+const { uploadImageMiddleware, uploadMedicalFileMiddleware } = require('../middleware/uploadMiddleware');
+const { uploadMedicalFile, viewMedicalFile, deleteMedicalFile }  = require('../controllers/authController');
+
 
 
 // Forgot password route
 router.post('/change-password', changePassword);
 
 // Route to upload profile picture
-router.post('/upload-profile-picture', authMiddleware,uploadImageMiddleware, roleCheck(['upload_profile_picture']), uploadProfilePicture);
+router.post('/profile-picture', authMiddleware,uploadImageMiddleware, uploadProfilePicture);
+router.get('/profile-picture', authMiddleware, viewProfilePicture);
+router.delete('/profile-picture', authMiddleware, deleteProfilePicture);
+
+router.post('/medical-file', authMiddleware, uploadMedicalFileMiddleware, uploadMedicalFile);
+router.get('/medical-file/:patientId', authMiddleware, viewMedicalFile);
+router.delete('/medical-file/:fileIndex', authMiddleware, deleteMedicalFile);
 
 
 module.exports = router;
