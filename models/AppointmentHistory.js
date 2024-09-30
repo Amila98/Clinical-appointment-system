c9934ff6eb5b1150d6b7ed88b1e575e37ab0e716
Amila98 <amilamadhujith@gmail.com>
2024-09-30 12:46:56 +0530

    updated

---
+++ models/AppointmentHistory.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const AppointmentHistorySchema = new mongoose.Schema({
+    appointment: { type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' },
+    doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+    specialization: { type: mongoose.Schema.Types.ObjectId, ref: 'Specialization', required: true },
+    day: { type: String, required: true },
+    timeSlot: {
+        start: { type: String, required: true },
+        end: { type: String, required: true }
+    },
+    treatmentPlan: {
+        type: String,  // Stores the treatment plan entered by the doctor
+        required: true,
+    },
+    prescription: {
+        date: {
+            type: Date,
+            required: false,
+        },
+        medicines: [{
+            type: String,
+        }]
+    },
+    status: { type: String, enum: ['Completed'], default: 'Completed' },  // Set status to 'Completed'
+    created_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('AppointmentHistory', AppointmentHistorySchema);
